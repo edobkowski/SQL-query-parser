fac5a0e573a011458902fcfd2e234d9ff4e4f402
edobkowski <edobkowski@gmail.com>
2018-10-30 13:55:04 +0100

    Add test cases for different conditions

--- src/test/java/com/codecool/predicates/PredicateFactoryTest.java
+++ src/test/java/com/codecool/predicates/PredicateFactoryTest.java
@@ -35,11 +35,41 @@ public class PredicateFactoryTest {
     }
 
     @Test
-    void getPredicates() throws IncorrectQueryException, IncorrectOperandException {
+    void getResultSet_SingleCondition() throws IncorrectQueryException, IncorrectOperandException {
+        List<List<Condition>> conditions = new ArrayList(Arrays.asList(new ArrayList(Arrays.asList(new Condition(
+                "name", "=",
+         "Eliza")))));
+        Predicate<String> predicate = this.predicateFactory.getPredicate(conditions);
+        List<String> expectedResult = new ArrayList<>(Arrays.asList("Eliza, Pistacjowa, 30, F",
+                "Eliza, Laskowa, 30, F"));
+        List<String> actualResult = data.filter(predicate).collect(Collectors.toList());
+
+        assertAll(() -> {
+            assertTrue(expectedResult.containsAll(actualResult));
+            assertTrue(actualResult.containsAll(expectedResult));
+        });
+    }
+
+    @Test
+    void getResultSet_WithOrAnd1() throws IncorrectQueryException, IncorrectOperandException {
         List<List<Condition>> conditions = this.getCond1();
         Predicate<String> predicate = this.predicateFactory.getPredicate(conditions);
 
         List<String> expectedResult = getExpectedResultForCond1();
+        List<String> actualResult = data.filter(predicate).collect(Collectors.toList());
+
+        assertAll(() -> {
+            assertTrue(expectedResult.containsAll(actualResult));
+            assertTrue(actualResult.containsAll(expectedResult));
+        });
+    }
+
+    @Test
+    void getResultSet_WithOrAnd2() throws IncorrectQueryException, IncorrectOperandException {
+        List<List<Condition>> conditions = this.getCond2();
+        Predicate<String> predicate = this.predicateFactory.getPredicate(conditions);
+
+        List<String> expectedResult = getExpectedResultForCond2();
         List<String> actualResult = data.filter(predicate).collect(Collectors.toList());
 
         assertAll(() -> {
@@ -53,6 +83,18 @@ public class PredicateFactoryTest {
                 new Condition("age", ">", "35"),
                 new Condition("age", "<", "60")));
         List<Condition> group2 = new ArrayList<>(Arrays.asList(
+                new Condition("sex", "=", "F")
+        ));
+
+        return new ArrayList<List<Condition>>(Arrays.asList(group1, group2));
+    }
+
+    private List<List<Condition>> getCond2() throws IncorrectOperandException {
+        List<Condition> group1 = new ArrayList<>(Arrays.asList(
+                new Condition("age", ">", "35"),
+                new Condition("age", "<", "60")));
+        List<Condition> group2 = new ArrayList<>(Arrays.asList(
+                new Condition("sex", "=", "F"),
                 new Condition("name", "=", "Eliza")
         ));
 
@@ -63,7 +105,18 @@ public class PredicateFactoryTest {
         return new ArrayList<>(Arrays.asList(
                 "Stanisław, Lagunowski, 40, M",
                 "Tomasz, Pies, 54, M",
-                "Eliza, Pistacjowa, 30, F"
+                "Eliza, Pistacjowa, 30, F",
+                "Eliza, Laskowa, 30, F",
+                "Agnieszka, Biegunowska, 68, F"
+        ));
+    }
+
+    private List<String> getExpectedResultForCond2() {
+        return new ArrayList<>(Arrays.asList(
+                "Stanisław, Lagunowski, 40, M",
+                "Tomasz, Pies, 54, M",
+                "Eliza, Pistacjowa, 30, F",
+                "Eliza, Laskowa, 30, F"
         ));
     }
 }
