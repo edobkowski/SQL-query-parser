cc2ec31f40510bd3683622672b6cadc5d53da53d
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-30 11:41:28 +0100

    Create IncorrectOperandException class

---
+++ src/main/java/com/codecool/exceptions/IncorrectOperandException.java
@@ -0,0 +1,7 @@
+package com.codecool.exceptions;
+
+public class IncorrectOperandException extends Exception {
+    public IncorrectOperandException(String message) {
+        super(message);
+    }
+}
