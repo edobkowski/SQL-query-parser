6a89e3ab2de7fed51da41a8597c04efc283dc695
edobkowski <edobkowski@gmail.com>
2018-10-31 11:11:28 +0100

    Add test case for getting condition list

---
+++ src/test/java/com/codecool/factories/ConditionFactoryTest.java
@@ -0,0 +1,38 @@
+package com.codecool.factories;
+
+import com.codecool.models.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionFactoryTest {
+
+    @Test
+    void getConditionList() {
+        ConditionFactory conditionFactory = new ConditionFactory();
+        String condition = buildCondition();
+        System.out.println(condition);
+        int expectedSize = 5;
+        int actualSize = conditionFactory.getConditionList(condition).size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    private String buildCondition() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 10; i++) {
+            sb.append("cond" + i + " = 10");
+            if(i == 9) {
+                sb.append(";");
+            } else if(i%2 == 0) {
+                sb.append(" and ");
+            } else {
+                sb.append(" or ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
