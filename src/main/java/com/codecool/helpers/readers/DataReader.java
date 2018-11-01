21887890a5948c6c534b2bc5b8d945d0a2b8ae5d
edobkowski <edobkowski@gmail.com>
2018-11-02 00:44:50 +0100

    Add DataReader interface

---
+++ src/main/java/com/codecool/helpers/readers/DataReader.java
@@ -0,0 +1,11 @@
+package com.codecool.helpers.readers;
+
+import com.codecool.exceptions.DataReaderException;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface DataReader {
+    List<String> getHeader() throws DataReaderException;
+    Stream<String> getDataStream() throws DataReaderException;
+}
