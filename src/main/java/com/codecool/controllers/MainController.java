5acb70d5f18f1538c6d6d06c36e357423870d3c4
edobkowski <edobkowski@gmail.com>
2018-11-01 21:17:27 +0100

    Refactor: remove unnecessary imports

--- src/main/java/com/codecool/controllers/MainController.java
+++ src/main/java/com/codecool/controllers/MainController.java
@@ -10,12 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
