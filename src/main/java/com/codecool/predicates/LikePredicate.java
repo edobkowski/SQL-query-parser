05f6156a26670d52e258bfe0a92f1cf241087984
reggae-krk <monika.gorecka.agh@gmail.com>
2018-11-02 23:58:32 +0100

    Add buildRegex method

--- src/main/java/com/codecool/predicates/LikePredicate.java
+++ src/main/java/com/codecool/predicates/LikePredicate.java
@@ -14,4 +14,15 @@ public class LikePredicate implements Predicate<String> {
         this.expectedValue = expectedValue;
     }
 
+    private Pattern buildRegex() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < expectedValue.length(); i++) {
+            if (expectedValue.charAt(i) == '%') sb.append("[\\w ]*");
+            else if (expectedValue.charAt(i) == '_') sb.append("[\\w]{1}");
+            else sb.append(expectedValue.charAt(i));
+        }
+        return Pattern.compile(sb.toString());
+    }
+
 }
