72fd160fc3435657fd474dff617df4ca45f0a54a
edobkowski <edobkowski@gmail.com>
2018-11-04 17:50:49 +0100

    Refactor: remove unnecassary imports

--- src/main/java/com/codecool/controllers/MainController.java
+++ src/main/java/com/codecool/controllers/MainController.java
@@ -4,8 +4,6 @@ import com.codecool.exceptions.DataReaderException;
 import com.codecool.exceptions.IncorrectQueryException;
 import com.codecool.services.QueryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
