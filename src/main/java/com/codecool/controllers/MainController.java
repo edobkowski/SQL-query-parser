289da1792509b4affc733c1b2140cb8e87ace9b8
edobkowski <edobkowski@gmail.com>
2018-11-04 17:02:30 +0100

    Refactor: extract entry point of app into another class

--- src/main/java/com/codecool/controllers/MainController.java
+++ src/main/java/com/codecool/controllers/MainController.java
@@ -2,7 +2,7 @@ package com.codecool.controllers;
 
 import com.codecool.exceptions.DataReaderException;
 import com.codecool.exceptions.IncorrectQueryException;
-import com.codecool.factories.QueryFactory;
+import com.codecool.services.QueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,15 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@SpringBootApplication
 public class MainController {
 
     @Autowired
-    private QueryHandler queryHandler;
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MainController.class, args);
-    }
+    private QueryService queryService;
 
     @GetMapping("/")
     public String handleGet(Model model) {
@@ -34,7 +29,7 @@ public class MainController {
 
     @PostMapping("/")
     public String handlePost(Model model, @RequestParam("stringQuery") String stringQuery) throws IncorrectQueryException, DataReaderException {
-        List<String> resultSet = this.queryHandler.executeQuery(stringQuery);
+        List<String> resultSet = this.queryService.executeQuery(stringQuery);
         model.addAttribute("records", resultSet);
 
         return "index";
