c3d86c4c2bc1412a871920bc683c9130ca42ad67
edobkowski <edobkowski@gmail.com>
2018-11-04 19:53:51 +0100

    Cleancode: remove intend

--- src/main/java/com/codecool/google/SheetsServiceUtil.java
+++ src/main/java/com/codecool/google/SheetsServiceUtil.java
@@ -12,11 +12,11 @@ public class SheetsServiceUtil {
     private static final String APPLICATION_NAME = "SQL QUERY PARSER";
 
     public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
-            Credential credential = GoogleAuthorizeUtil.authorize();
-            return new Sheets.Builder(
-                    GoogleNetHttpTransport.newTrustedTransport(),
-                    JacksonFactory.getDefaultInstance(), credential)
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
+        Credential credential = GoogleAuthorizeUtil.authorize();
+        return new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 }
