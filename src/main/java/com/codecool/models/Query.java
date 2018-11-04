c533481b2f1273b076bbb08ab060cdf43c5753b2
edobkowski <edobkowski@gmail.com>
2018-11-04 16:12:03 +0100

    Override equals() and hasCode() methods

--- src/main/java/com/codecool/models/Query.java
+++ src/main/java/com/codecool/models/Query.java
@@ -4,6 +4,7 @@ import com.codecool.helpers.enums.QueryType;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Query {
@@ -67,5 +68,22 @@ public class Query {
         String s = listOfConditions.stream().flatMap(Collection::stream).collect(Collectors.toList()).toString();
         return queryType + (isAll ? " * " : columnList.stream().collect(Collectors.joining(", ", " ", " "))) + "FROM " + source + " WHERE "
                 + (listOfConditions.isEmpty() ? ";" : s);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Query query = (Query) o;
+        return isAll == query.isAll &&
+                queryType == query.queryType &&
+                Objects.equals(columnList, query.columnList) &&
+                Objects.equals(listOfConditions, query.listOfConditions) &&
+                Objects.equals(source, query.source);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(queryType, columnList, isAll, listOfConditions, source);
     }
 }
