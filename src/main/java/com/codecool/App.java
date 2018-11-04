04ca409fa8b54672b6f79ea0110835043a6915dc
edobkowski <edobkowski@gmail.com>
2018-11-04 17:49:18 +0100

    Authorise app at entry point

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,5 +1,6 @@
 package com.codecool;
 
+import com.codecool.google.GoogleAuthorizeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
     public static void main(String[] args) throws Exception {
+        GoogleAuthorizeUtil.authorize();
         SpringApplication.run(App.class, args);
     }
 }
