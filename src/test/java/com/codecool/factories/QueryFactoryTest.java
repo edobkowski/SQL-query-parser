38fd3f2bef2ba999a2003946ad3c633259b1606b
edobkowski <edobkowski@gmail.com>
2018-11-04 16:17:53 +0100

    Add test case for proper query

--- src/test/java/com/codecool/factories/QueryFactoryTest.java
+++ src/test/java/com/codecool/factories/QueryFactoryTest.java
@@ -1,9 +1,15 @@
 package com.codecool.factories;
 
+import com.codecool.exceptions.IncorrectOperandException;
 import com.codecool.exceptions.IncorrectQueryException;
+import com.codecool.helpers.enums.QueryType;
+import com.codecool.models.Condition;
 import com.codecool.models.Query;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +22,17 @@ class QueryFactoryTest {
     void prepareTest() {
         this.queryFactory = new QueryFactory(conditionFactory);
     }
-    
+
+    @Test
+    void testProperQueryBuilding() throws IncorrectQueryException, IncorrectOperandException {
+        Condition condition = new Condition("age", ">", "15");
+        Query expected = new Query(QueryType.SELECT, Arrays.asList("*"), "file",
+                Arrays.asList(Arrays.asList(condition)));
+        Query actual = queryFactory.createQuery("select * from file where age > 15;");
+
+        assertEquals(expected, actual);
+    }
+
     @Test
     void passWrongQuery_ShouldThrowException() {
         assertAll(() -> {
