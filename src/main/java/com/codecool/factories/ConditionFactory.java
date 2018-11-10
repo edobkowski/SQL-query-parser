ccf52c6f59b6712739b30bb645e019309d84bb0e
reggae-krk <monika.gorecka.agh@gmail.com>
2018-11-10 15:45:21 +0100

    Change nested for loops to stream

--- src/main/java/com/codecool/factories/ConditionFactory.java
+++ src/main/java/com/codecool/factories/ConditionFactory.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -83,34 +84,31 @@ public class ConditionFactory {
     }
 
     private List<List<Condition>> getConditions(List<List<String>> stringConditions) {
-        List<List<Condition>> list = new ArrayList<>();
         String patternString = "(\\w+)[ ]?(<|>|<>|={1,2})[ ]?('\\w+'|\\w+)|(\\w+)[ ](LIKE|like){1}[ ]'([%]?\\w+[%]?)'";
         Pattern pattern = Pattern.compile(patternString);
 
-        for (int x = 0; x < stringConditions.size(); x ++) {
-            List<Condition> innerList = new ArrayList<>();
-            for (int z = 0; z < stringConditions.get(x).size(); z++) {
-                String conditionString = stringConditions.get(x).get(z);
-                Matcher matcher = pattern.matcher(conditionString);
-
-                if (matcher.matches()) {
-
-                    for (int i = 1; i <= matcher.groupCount(); i++) {
-                        if(matcher.group(i)==null) continue;
-                        try {
-                            innerList.add(new Condition(matcher.group(i), matcher.group(i+1), matcher.group(i+2)));
-                        }
-                        catch (IncorrectOperandException e) {
-                            break;
-                        }
-                        break;
-                    }
-                }
-            }
-            list.add(innerList);
-
-        }
-        return list;
+        return stringConditions.stream()
+                .map(x -> {
+                    List<Condition> innerList = new ArrayList<>();
+                    x.stream()
+                        .forEach(condition -> {
+                            Matcher matcher = pattern.matcher(condition);
+                            if (matcher.matches()) {
+                                for (int i = 1; i <= matcher.groupCount(); i++) {
+                                    if(matcher.group(i)==null) continue;
+                                    try {
+                                        innerList.add(new Condition(matcher.group(i), matcher.group(i+1), matcher.group(i+2)));
+                                    }
+                                    catch (IncorrectOperandException e) {
+                                        break;
+                                    }
+                                    break;
+                                }
+                            }
+                            });
+                    return innerList;
+                })
+                .collect(Collectors.toList());
     }
 
     public List<List<Condition>> getConditionList(String condition) {
